156d240dd71faa4e35dd55bffb5d45cbc16aee4b
NATACHAOS <164019299+NATACHAOS@users.noreply.github.com>
2024-06-10 16:56:51 +0300

    Add files via upload

--- with_pomesch.py
+++ with_pomesch.py
@@ -1,10 +1,10 @@
 file_name = 'pomesch.txt'
 with open(file_name, mode='r', encoding='utf8') as file:
     for line in file:
-        print(line.upper()[:1] + '.' + list[1:])
+        print(line.upper().strip())
 
 
-# как сделать все буквы в верхнем регистре и после второго символа поставить во всех элементах точку
+# как после второго символа поставить во всех элементах точку
 # было 3б8.1     стало 3Б.8.1
 #      2б1             2Б.1
 
